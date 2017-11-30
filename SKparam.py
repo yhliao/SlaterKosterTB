614159278315df9e11a7b7ebf2de64016067a9ec
Henry Liao <henryl1152@gmail.com>
2017-11-29 18:07:20 -0800

    Updated the Parameters from Varun's TED paper

--- SKparam.py
+++ SKparam.py
@@ -1,14 +1,36 @@
+########################################################
+### S-K parameters from Varun's paper
+## "    Screening in Ultrashort (5nm) Channel MoS2 
+##   Transistors: A Full-Band Quantum Transport Study"
+## IEEE TED, Vol.62, No. 8, Aug. 2015
+########################################################
 
 SS = {
   "ss_sigma": -0.8590,
-  #TODO .........
+  "sp_sigma": -0.2142,
+  "pp_sigma":  0.8715,
+  "pp_pi"   : -0.2449,
+  "sd_sigma":  3.1818,
+  "pd_sigma":  0.1138,
+  "pd_pi"   : -0.4476,
+  "dd_sigma":  3.7203,
+  "dd_pi"   : -2.5901,
+  "dd_delta": -1.1719,
   "flipsite": SS
 }
 
 
 MoMo = {
   "ss_sigma": -1.5166,
-  #TODO .........
+  "sp_sigma":  0.4991,
+  "pp_sigma": -3.8198,
+  "pp_pi"   :  4.5562,
+  "sd_sigma":0.007971,
+  "pd_sigma":  1.3306,
+  "pd_pi"   : 0.95906,
+  "dd_sigma": 0.95906,
+  "dd_pi"   : -0.4520,
+  "dd_delta":  0.5143,
   "flipsite": MoMo
 }
 
