292b6ee5e48e338029984866ac219ae335b02a36
Ming-Yen Kao <mingyenkao@gmail.com>
2017-12-07 17:01:36 -0800

    Correct p(Mo)d(Mo)pi

--- MoS2TB.py
+++ MoS2TB.py
@@ -25,7 +25,7 @@ def H(K):
    assert np.isclose(np.sum(H-H.H),0)
    return H
 
-a=3.16
+a=3.18
 c=3.12
 
 d00 = [0, 0]
@@ -38,6 +38,8 @@ H00.fill_SK("Mo","S_u", lmn([0,a/sqrt(3),c/2]),MoS)
 H00.fill_SK("Mo","S_d", lmn([0,a/sqrt(3),-c/2]),MoS)
 H00.fill_SK("S_u","Mo", lmn([0,-a/sqrt(3),-c/2]),SMo)
 H00.fill_SK("S_d","Mo", lmn([0,-a/sqrt(3),c/2]),SMo)
+H00.fill_SK("S_u","S_d", lmn([0,0,-1]),SS)
+H00.fill_SK("S_d","S_u", lmn([0,0,1]),SS)
 
 d01 = [-1./2, sqrt(3)/2]
 H01 = TBblock(d01)
@@ -55,14 +57,7 @@ H04.fill_SK("S_u","S_u",[1./2,-sqrt(3)/2,0],SS)
 H04.fill_SK("S_d","S_d",[1./2,-sqrt(3)/2,0],SS)
 H04.fill_SK("Mo","S_u",lmn([a/2,-a/(2*sqrt(3)),c/2]),MoS)
 H04.fill_SK("Mo","S_d",lmn([a/2,-a/(2*sqrt(3)),-c/2]),MoS)
-'''
-### H04
-H04.fill_SK("Mo" ,"Mo" ,[1./2,-sqrt(3)/2,0],MoMo)
-H04.fill_SK("S_u","S_u",[1./2,-sqrt(3)/2,0],SS)
-H04.fill_SK("S_d","S_d",[1./2,-sqrt(3)/2,0],SS)
-H04.fill_SK("Mo","S_u",[-0.7765526458,0.4483428791,0.4426676528],MoS)
-H04.fill_SK("Mo","S_d",[-0.7765526458,0.4483428791,-0.4426676528],MoS)
-'''
+
 
 d02 = [-1,0]
 H02 = TBblock(d02)
@@ -95,25 +90,19 @@ H06.fill_SK("S_u","S_u",[1./2,sqrt(3)/2,0],SS)
 H06.fill_SK("S_d","S_d",[1./2,sqrt(3)/2,0],SS)
 H06.fill_SK("S_u","Mo",lmn([a/2,a/(2*sqrt(3)),-c/2]),SMo)
 H06.fill_SK("S_d","Mo",lmn([a/2,a/(2*sqrt(3)), c/2]),SMo)
-'''
-### H06
-H06.fill_SK("Mo" ,"Mo" ,[1./2,sqrt(3)/2,0],MoMo)
-H06.fill_SK("S_u","S_u",[1./2,sqrt(3)/2,0],SS)
-H06.fill_SK("S_d","S_d",[1./2,sqrt(3)/2,0],SS)
-H06.fill_SK("S_d","Mo",[0.7765526458,0.4483428791,0.4426676528],SMo)
-H06.fill_SK("S_u","Mo",[0.7765526458,0.4483428791,-0.4426676528],SMo)
-'''
+
+
 
 print lmn([a/2,a/(2*sqrt(3)),-c/2])
 E_GM = np.zeros([1000,H_dim])
-K_GM = np.linspace(-np.pi,np.pi,1000)
+K_GM = np.linspace(-2*np.pi,2*np.pi,1000)
 for i,kx in enumerate(K_GM):
-   k   = [0,kx]
+   k   = [kx,0]
    E,_ = eigh(H(k))
    E_GM[i,:] = E
 
 plt.plot(K_GM,E_GM[:,:])
 plt.ylim([-5,5])
-
+plt.grid()
 plt.show()
 
